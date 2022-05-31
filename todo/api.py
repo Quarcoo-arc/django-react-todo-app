d3639f4961e79cd4e9bf2ada97f834febbed4dfb
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-05-31 10:00:02 +0000

    Todo viewset added

---
+++ todo/api.py
@@ -0,0 +1,9 @@
+from todo.models import Todo
+from rest_framework import viewsets, permissions
+from .serializers import TodoSerializer
+
+#Todo Viewset
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = TodoSerializer
